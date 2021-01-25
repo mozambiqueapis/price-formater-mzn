7c0b124c45f8270c9894d6c16cf8c48d026c0fd2
Albertino Augusto <albertinoaugusto.it@gmail.com>
2021-01-25 13:36:13 +0200

    Teste

---
+++ main.js
@@ -0,0 +1,6 @@
+const numberFormater = (num) => {
+    var p = num.toFixed(2).split(".");
+    return "" + p[0].split("").reverse().reduce(function (acc, num, i, orig) {
+        return num == "-" ? acc : num + (i && !(i % 3) ? "," : "") + acc;
+    }, "") + "." + p[1];
+}
